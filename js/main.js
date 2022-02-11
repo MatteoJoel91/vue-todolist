23af78ab7e4cb00975cb09abc79fd171f49cdd5c
Matteo Gioele Cocomazzi <TheZefyro@gmail.com>
2022-02-11 15:54:48 +0100

    push

---
+++ js/main.js
@@ -0,0 +1,46 @@
+const root = new Vue (
+    {
+        el: "#root",
+        data: {
+            newtodo: '',
+            done: false,
+            todos: [
+                {
+                    text: 'studiare',
+                    done: false,
+                },
+                {
+                    text: 'mangiare',
+                    done: false,
+                },
+                {
+                    text: 'giocare',
+                    done: false,
+                },
+                {
+                    text: 'dormire',
+                    done: false,
+                },
+            ]
+        },
+
+      
+        methods: {
+            addTodo(){
+                this.todos.push(
+                    {
+                        text: this.newtodo,
+                        done: false,
+                    }
+                );
+                this.newtodo = '';
+            },
+           removeTodo(indice){
+            this.todos.splice(indice, 1);
+           },
+           todoFatta(){
+            this.done = !this.done;
+           },        
+        }
+    }  
+);
